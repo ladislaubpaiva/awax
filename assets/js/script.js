3338256f09dd28195d0c69575ff0a32d68d2f7ec
Ladislau Paiva <ladislaubpaiva@hotmail.com>
2022-05-08 16:10:26 +0100

    end css tablet and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
-document.querySelector('#root').addEventListener('click', () => {
-  console.log('ola mundo');
+const btnOpen = document.querySelector('.bx-menu');
+const btnClose = document.querySelector('.bx-x');
+const nav = document.querySelector('.menu nav');
+btnOpen.addEventListener('click', () => {
+  btnOpen.style.display = 'none';
+  nav.style.display = 'flex';
+  btnClose.style.display = 'block';
 });
+btnClose.addEventListener('click', () => {
+  btnClose.style.display = 'none';
+  nav.style.display = 'none';
+  btnOpen.style.display = 'block';
+});
+
+nav.querySelectorAll('ul li').forEach((e) => {
+  e.addEventListener('click', (el) => {
+    removeActive();
+    el.target.classList.add('active');
+  });
+});
+const removeActive = () => {
+  nav.querySelectorAll('ul li a').forEach((e) => {
+    e.classList.remove('active');
+  });
+};
