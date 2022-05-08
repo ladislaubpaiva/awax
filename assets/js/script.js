29d7822f19fcec158e2e602e5870f487647ad384
Ladislau Paiva <ladislaubpaiva@hotmail.com>
2022-05-08 23:35:47 +0100

    banner slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const btnOpen = document.querySelector('.bx-menu');
 const btnClose = document.querySelector('.bx-x');
 const nav = document.querySelector('.menu nav');
+
+// || Events
 btnOpen.addEventListener('click', () => {
   btnOpen.style.display = 'none';
   nav.style.display = 'flex';
@@ -12,14 +14,25 @@ btnClose.addEventListener('click', () => {
   btnOpen.style.display = 'block';
 });
 
-nav.querySelectorAll('ul li').forEach((e) => {
-  e.addEventListener('click', (el) => {
-    removeActive();
-    el.target.classList.add('active');
+document.querySelectorAll('.banner .slider-pointers .pointer').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    document.querySelectorAll('.banner .slider-pointers .pointer').forEach((el) => {
+      el.classList.remove('active');
+    });
+    const key = e.currentTarget.getAttribute('data-slide');
+    switch (key) {
+      case '1':
+        document.querySelector('.banner .sliders').style.marginLeft = '0';
+        e.currentTarget.classList.add('active');
+        break;
+      case '2':
+        document.querySelector('.banner .sliders').style.marginLeft = '-100vw';
+        e.currentTarget.classList.add('active');
+        break;
+      case '3':
+        document.querySelector('.banner .sliders').style.marginLeft = '-200vw';
+        e.currentTarget.classList.add('active');
+        break;
+    }
   });
 });
-const removeActive = () => {
-  nav.querySelectorAll('ul li a').forEach((e) => {
-    e.classList.remove('active');
-  });
-};
